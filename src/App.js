1e17b37666b3354a1a749d42e9c2e5971c9fe360
Muhammad Enamul Haque <muhammad.enamul.haque79@gmail.com>
2022-10-23 21:19:25 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -1,49 +1,54 @@
 import './App.css';
 import app from './firebase.init';
-import { getAuth } from 'firebase/auth';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import {
+  createUserWithEmailAndPassword,
+  getAuth
+} from 'firebase/auth';
+import { useState } from 'react';
 
 const auth = getAuth(app);
-function App() {
 
-  const handleEmailBlur = (event) => {
-    console.log(event.target.value);
+function App() {
+  const[user,setUser]=useState('');
+  const[email,setEmail]=useState('');
+  const[password,setPassword]=useState('');
+  // const handleEmailChange = () => {
+  //   console.log('email working!!');
+  // };
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
   };
 
-  const handlePasswordBlur = (event) => {
-    console.log(event.target.value);
+  // const handlePasswordChange = () => {
+  //   console.log('password working');
+  // };
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
   };
 
   const handleFormSubmit = (event) => {
-    console.log('form submitted');
+    console.log('form submited');
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result)=>{
+        const user=result.user;
+        console.log(user);
+        setUser(user);
+       
+      }).catch((error)=>{
+        console.error(error);
+      });
+
     event.preventDefault();
   };
 
-return (
-    <div>
-      <div className="registration w-50 mx-auto mt-3">
-        <h2 className='text-primary'>Pleas Register!!</h2>
-        <Form onSubmit={handleFormSubmit}>
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
-          </Form.Group>
-
-          <Form.Group controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Password" />
-          </Form.Group>
-          <Form.Group controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-          </Form.Group>
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
+  return (
+    <div className='App'>
+      <div className="registration">
+        <form onSubmit={handleFormSubmit}>
+          <input onChange={handleEmailChange} type="email" name="email" id="" />
+          <input onChange={handlePasswordChange} type="password" name="password" id="" />
+          <input type="submit" value="Login" />
+        </form>
       </div>
     </div>
   );
--- src/__Steps.js
+++ src/__Steps.js
@@ -1,11 +1,15 @@
-//Steps are creating a project into firebase;
 /*
- 1.Create a project(Select a reliable name);
- 2.Register app;
- 3.npm install firebase**;
- 4.initialized firebase into firebase.init.js;and export default app;
- 5.Authentication (adding sign-in desire method and enable provider);
- 6.const auth=getAuth(app) and import {getAuth} from "firebase/auth";
- 7.react-bootsrap...........;
- 8.
- */
+1.Create a project;
+2.Register app;
+3.Authenticate with desire provider and enable it;
+4.npm install firebase;
+5.initialized firebase into firebase.init.js;
+6.export default app;
+7.const auth=getAuth(app) & import app from './firebase.init.js;
+8.import {getAuth} from 'firebase/auth';
+9.const handleEmailChange=()=>{
+    console.log('email working');
+};connect with button event handler;
+10.
+
+*/
