cfe78faad9c2310373dcba7be85166cc2cc758a1
Muhammad Enamul Haque <muhammad.enamul.haque79@gmail.com>
2022-10-23 22:57:39 +0600

    validated

--- src/App.js
+++ src/App.js
@@ -11,10 +11,11 @@ import { useState } from 'react';
 const auth = getAuth(app);
 
 function App() {
-  const[user,setUser]=useState('');
-  const[email,setEmail]=useState('');
-  const[password,setPassword]=useState('');
- 
+  const [validated, setValidated] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
   const handleEmailBlur = (event) => {
     setEmail(event.target.value);
   };
@@ -24,43 +25,61 @@ function App() {
   };
 
   const handleFormSubmit = (event) => {
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((result)=>{
-        const user=result.user;
-        setUser(user);
-      }).catch((error)=>{
-        console.error(error);
-       
-      });
-        
-
     event.preventDefault();
-  };
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+      return;
+    }
+    if(!/(?=.*[!@#$%^&*])/.test(password)){
+        return;
+    };
 
-  return (
+    setValidated(true);
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        setError('');
+      }).catch((error) => {
+        console.error(error);
+        setError(error.message);
+      });
+    };
+
+return (
     <div>
       <div className="registration w-50 mx-auto mt-2">
         <h2 className='text-primary'>Please Register</h2>
-      <Form onSubmit={handleFormSubmit}>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
+        <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" required />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+            <Form.Control.Feedback type="invalid">
+              Please provide a valid email.
+            </Form.Control.Feedback>
+          </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Password" required />
+            <Form.Control.Feedback type="invalid">
+              Please provide a valid password.
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          <p className='text-danger'>{error}</p>
+          <Button variant="primary" type="submit">
+            Register
+          </Button>
+        </Form>
       </div>
     </div>
   );
