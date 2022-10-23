220ffdb4c7ef336e555ede4c9c22bb790f1086f0
Muhammad Enamul Haque <muhammad.enamul.haque79@gmail.com>
2022-10-24 01:05:19 +0600

    toggle

--- src/App.js
+++ src/App.js
@@ -12,9 +12,11 @@ const auth = getAuth(app);
 
 function App() {
   const [validated, setValidated] = useState(false);
+  const [registered, setRegistered] = useState(false);
+  const [error, setError] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+
 
   const handleEmailBlur = (event) => {
     setEmail(event.target.value);
@@ -24,35 +26,44 @@ function App() {
     setPassword(event.target.value);
   };
 
+  const handleRegisteredChange=(event)=>{
+    // console.log(event.target.checked);
+    setRegistered(event.target.checked);
+  }
+ 
+
   const handleFormSubmit = (event) => {
     event.preventDefault();
     const form = event.currentTarget;
     if (form.checkValidity() === false) {
-      event.preventDefault();
       event.stopPropagation();
       return;
     }
-    if(!/(?=.*[!@#$%^&*])/.test(password)){
-        return;
-    };
+    if (!/(?=.*[!@#$%^&*])/.test(password)) {
+      setError("Your password must contain at least one special character");
+      return;
 
+    };
     setValidated(true);
+    setError('');
 
     createUserWithEmailAndPassword(auth, email, password)
       .then((result) => {
         const user = result.user;
         console.log(user);
-        setError('');
+        setEmail('');
+        setPassword('');
       }).catch((error) => {
         console.error(error);
         setError(error.message);
       });
-    };
+    event.preventDefault();
+  };
 
-return (
+  return (
     <div>
       <div className="registration w-50 mx-auto mt-2">
-        <h2 className='text-primary'>Please Register</h2>
+        <h2 className='text-primary'>Please {registered ? 'Login' : 'Register'}!!</h2>
         <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
@@ -72,12 +83,14 @@ return (
               Please provide a valid password.
             </Form.Control.Feedback>
           </Form.Group>
+
           <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
+            <Form.Check onChange={handleRegisteredChange} type="checkbox" label="Already Registered?" />
           </Form.Group>
+
           <p className='text-danger'>{error}</p>
           <Button variant="primary" type="submit">
-            Register
+            {registered ? 'Login' : 'Register'}
           </Button>
         </Form>
       </div>
